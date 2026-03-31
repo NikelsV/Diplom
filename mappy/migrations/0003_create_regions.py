d54f7a8afbd716e6531cddeeb78bdda6b98fb2af
Nikita Sharonov <nikita.scharonow@yandex.ru>
2026-03-31 14:29:33 +0700

    Исправлен баг с неправильной группировкой отдельных кусочков карты

--- mappy/migrations/0003_create_regions.py
+++ mappy/migrations/0003_create_regions.py
@@ -5,10 +5,7 @@ def create_regions(apps, schema_editor):
     Region = apps.get_model('mappy', 'Region')
     Region.objects.all().delete()
     for i in range(1, 154):
-        Region.objects.create(
-            name=f'Регион {i}',
-            svg_id=f'r{i}',
-        )
+        Region.objects.create(name=f'Регион {i}', svg_id=f'r{i}')
 
 
 def delete_regions(apps, schema_editor):
@@ -17,11 +14,9 @@ def delete_regions(apps, schema_editor):
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('mappy', '0002_connection_waypoints_devicetype_builtin_icon_and_more'),
     ]
-
     operations = [
         migrations.RunPython(create_regions, delete_regions),
     ]
